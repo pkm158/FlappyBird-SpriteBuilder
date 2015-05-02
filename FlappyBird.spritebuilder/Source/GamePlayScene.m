3109ff4ef58ece165604a7ff8280363f43a5a66d
Kemi Peng <kermit@brandeis.edu>
2015-05-01 22:33:34 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -28,11 +28,6 @@
 // put new methods here
 -(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
     [character flap];
-    printf("%d", [self plus:5 and:2]);
-}
-
--(int) plus:(int)value and:(int)value2{
-    return value + value2;
 }
 
 @end
